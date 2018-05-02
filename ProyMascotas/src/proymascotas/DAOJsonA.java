6cd160852f1171e79a0343264c68d6ed7aa9e182
MonicaPol <cuadernodeproyectos@gmail.com>
2018-05-02 11:20:31 -0300

    Subo codigo para agregar en Proymascotas.java

---
+++ ProyMascotas/src/proymascotas/DAOJsonA.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proymascotas;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+ 
+
+/**
+ *
+ * @author Nina
+ */
+public class DAOJson {
+ 
+public DAOJson(Mascota unaMascota) {
+    
+JSONObject obj = new JSONObject();
+String nombre = unaMascota.getNombre();
+float energia = unaMascota.getEnergia();
+float energiaIdeal = unaMascota.getEnergiaIdeal();
+obj.put ("nombre", nombre);
+obj.put ("energia", energia);
+obj.put ("energiaIdeal", energiaIdeal);
+
+try (FileWriter file = new FileWriter("mascotita.json"))
+{
+	file.write(obj.toString());
+	file.flush();
+}
+catch (IOException e)
+{
+//e.printStackTrace();
+}
+}
+    
+}
+    
+    
+
